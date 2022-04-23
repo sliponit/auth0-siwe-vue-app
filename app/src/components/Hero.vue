f5338999650e552d0b934823c30d519e5c7c8f33
sliponit <sliponit@pm.me>
2022-04-24 00:00:42 +0200

    verbiage

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-center hero">
     <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
-    <h1 class="mb-4">Vue.js Sample Project</h1>
+    <h1 class="mb-4">OIDC SIWE</h1>
     <p class="lead">
-      This is a sample application that demonstrates an authentication flow for an SPA, using
+      This is a sample application that demonstrates an authentication flow using OIDC and SIWE
       <a
         href="https://vuejs.org"
       >Vue.js</a>
