ebc985bcfff7382218db592614185655ff3a275f
sliponit <sliponit@pm.me>
2022-04-24 02:21:09 +0200

    desc

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Submission for https://amsterdam.ethglobal.com/
 
-- [Diagram source](./sequence_diagram.txt)
-![Diagram](./sequence_diagram.png)
+An attempt at understanding a bit more the process when using [OpenID Connect](https://openid.net/specs/openid-connect-core-1_0.html) and [SIWE](https://login.xyz)
 
-- example app in [app](./app) deployed on cloudflare pages
+The submission is two-fold:
+
+- Mainly q protocol flow diagram [Diagram source](./sequence_diagram.txt)
+![Diagram](./sequence_diagram.png) heavily inspired by the one ine [Auth0 blog post](https://auth0.com/blog/sign-in-with-ethereum-siwe-now-available-on-auth0/)
+
+- An example app in [app](./app) deployed on [cloudflare pages](https://auth0.com/blog/sign-in-with-ethereum-siwe-now-available-on-auth0/)
--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -3,9 +3,14 @@
     <h1 class="mb-4">OIDC - SIWE</h1>
     <p class="lead">
       This is a sample application that demonstrates an authentication flow using
-      <a href="https://openid.net/specs/openid-connect-core-1_0.html">OpenID Connect</a> and <a href="https://eips.ethereum.org/EIPS/eip-4361">SIWE</a>
-      powered by <a href="https://auth0.com">Auth0</a> and <a href="https://login.xyz">Spruce ID</a>
-    </p>
+      <a href="https://openid.net/specs/openid-connect-core-1_0.html" target="_blank">OpenID Connect</a> and <a href="https://eips.ethereum.org/EIPS/eip-4361" target="_blank">SIWE</a>
+      powered by <a href="https://auth0.com" target="_blank" >Auth0</a> and <a href="https://login.xyz" target="_blank">Spruce ID</a><br><br>
+      First Login flow is described 
+      <a href="https://sequencediagram.org/index.html#initialData=C4S2BsFMAIHkEkAiBhaBleB1AotADgE4D2wRAxkeNAGbhEDuAUI3gIYGhkhsB2w0ycCEh9omSACNorPHhbtO3VqIQp8xAG4gAJpALQAFKwCuwABYAGaNqIBbViB4BKeRxBde-DDnVEtu-QMiHTIAOjoAc0dQgA8ATwAvFzY3D2V+QWFRTFZwKGBmRkyRfnEpGTwAWgA+VVRCPx09QxNzKxt7RycALmgAJUhtEAJIMn56MDNoAGdgVmAYFNZbSAWCAB0eAHpjLcjHAH5Z+cgAXmhoRh4SGD9m4uzJaVkAGjrff2ajU0trOwdnN1kEQeNMSjMyCMRIx3g1PoFWr8OgCnDVvLg4U1AsFtGF9jxYokev1BsNRsBNtAtgdrjwyGcAGTIxynBkjIYjMYAfWMBBAp1Cgr2RCi2zIuXAElYZAA1gzjgtWWQhCUuTpTlcbtA7vpYZosS0fu1-l0XuiPgagiFwiLovEkkCQWDRNNIZBodcFtqNM1zZiAoYcXjbQT7U4Xg9ShLVt0AApEabTEAScBxaD0aP8Cg8Hjkxh+-UBq24m2iwlJGqRsSZ7oDACOxkgsxmIAiPEqjnTk2gqzMekgxlsl09tx9+irOTyMYETvBrqhPGgnYzU4KE8zlTRWAxha+QdLdqJvQAqmD9NNjBJbGBpi22-NeZB89uLUX9-jy6jqgXGm-rR+w16AA1PQQGoNMk3vYBH2fHx-T3f8Q0-Go9V-L5EWNTpAU2ZAJSpfEtnFPIpVlA4KF0DVUPhQ02j+LCvwnJ4KiBPDqXIs4ihVR5ylkFCkHqXcESNOiUTjWA0AAFSpIhES2UgZRESkAG8ACJlSyYA1W0FSXjUohdC5McwOEAgdJUiICHSLlgDiPBIBU7oVMRIg+QSeYQBBLl2LM7zdPZMluV5EAHIAXxhfjX3Q4TmWcTdqkYni8G6aV6UTaB5JEaQeG0aBjDPJceGoIhGFOc5Y2IVLb1IaARmmSgfWgFNyBlMgzABax5lYbVF2wcx+0HLZsAAOTQaBSuYIA" target="_blank">
+        here
+      </a>
+    </p>    
+
   </div>
 </template>
 
